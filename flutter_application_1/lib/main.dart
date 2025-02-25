a088bfcdd4a94b8ab42193f353f6cf4948d4845f
Md Mehedi Hasan <70844877+MehedisCode@users.noreply.github.com>
2025-02-25 14:37:23 +0600

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -20,7 +20,7 @@ class HomeActivity extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text("Home 2"),
         backgroundColor: Colors.amber,
       ),
     );
