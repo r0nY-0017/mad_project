bd60f6bfe35d7ef23cdc8898a6650fbc630a29dd
Md Mehedi Hasan Rony <125382191+r0nY-0017@users.noreply.github.com>
2025-02-25 15:11:18 +0600

    Homepage

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,32 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeActivity(),
       );
   }
 }
 
-class HomeActivity extends StatelessWidget{
+class HomeActivity extends StatefulWidget {
+  @override
+  MyState createState() => MyState();
+}
+
+class MyState extends State<HomeActivity> {
+
+  SnackMessage(message, context){
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home 2"),
-        backgroundColor: Colors.amber,
-      ),
+        appBar: AppBar(
+          title: Text("Blood Donation"),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 225, 160, 156),
 
-      body: Center(
-        child: Text("Home Activity"),
-      ),
+        ),
+
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: "Donor List"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          ],
+
+          onTap: (value){
+            if(value == 0){
+              SnackMessage("Bottom Navigation Home Button is Pressed", context);
+            }
+            
+            else if(value == 1){
+              SnackMessage("Bottom Navigation Contact Button is Pressed", context);
+            }
+            
+            else{
+              SnackMessage("Bottom Navigation Profile Button is Pressed", context);
+            }
+          },
+          ),
+
+
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                DrawerHeader(child: Text("DrawerHeader1")),
+                DrawerHeader(child: Text("DrawerHeader2")),
+                DrawerHeader(child: Text("DrawerHeader3")),
+              ],
+            ),
+          ),
+
+          body:Center(
+            child: Text("Welcome to Blood Donation"),
+          ),
     );
   }
 }
